79c83f9ee6cd7957bd6772e9746312c0bb852de6
utkarshbhardwaj01 <67866657+utkarshbhardwaj01@users.noreply.github.com>
2020-10-31 18:32:19 +0530

    Initial Upload - Added Homepage, and Main Page without DB and PHP support

---
+++ JavaScript/main.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+  // Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
